8a171467f6da746140f03840698aa651ca384c06
213311012 <numangunduz00@gmail.com>
2024-03-01 16:06:01 +0300

    Katmanlar arası referans ve Migration ayarları

---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-EV2S3I4; database=ApiDB;integrated security = true; TrustServerCertificate=True");
+        }
+        public DbSet<Room>Rooms { get; set; }
+        public DbSet<Service>Services { get; set; }
+        public DbSet<Staff>Staff { get; set; }
+        public DbSet<Subscribe>Subscribes { get; set; }
+        public DbSet<Testimonial>Testimonials { get; set; }
+    }
+}
