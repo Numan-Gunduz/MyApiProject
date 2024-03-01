43ea65e12a25884c408d3f4e237c427a4f23f500
213311012 <numangunduz00@gmail.com>
2024-03-01 16:44:35 +0300

    GenericRepository ve Ef Classları

--- ApiConsume/HotelProject.DataAccessLayer/Abstract/IGenericDal.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Abstract/IGenericDal.cs
@@ -8,5 +8,11 @@ namespace HotelProject.DataAccessLayer.Abstract
 {
     public interface IGenericDal<T> where T:class
     {
+        void Insert (T  t);
+        void Delete (T  t);
+        void Update (T  t);
+        List<T> GetList ();
+
+        T GetByID(int id);
     }
 }
