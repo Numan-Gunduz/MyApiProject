daeaf147b05dd3b587e60a8e9f45f2f0a4940297
213311012 <numangunduz00@gmail.com>
2024-04-14 15:42:30 +0300

    Sayfaların Veri Tabanından Dinamik Veri Çekme İşlemleri Devamı

--- Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -4,6 +4,8 @@ using HotelProject.WebUI.Dtos.AboutDto;
 using HotelProject.WebUI.Dtos.LoginDto;
 using HotelProject.WebUI.Dtos.RegisterDto;
 using HotelProject.WebUI.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.StaffDto;
+using HotelProject.WebUI.Dtos.TestimonialDto;
 
 namespace HotelProject.WebUI.Mapping
 {
@@ -20,6 +22,9 @@ namespace HotelProject.WebUI.Mapping
 
             CreateMap<ResultAboutDto, About>().ReverseMap();
             CreateMap<UpdateAboutDto, About>().ReverseMap();
+
+            CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();
+            CreateMap<ResultStaffDto, Staff>().ReverseMap();
         }
     }
 }
--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.StaffDto;
+using HotelProject.WebUI.Dtos.TestimonialDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _TeamPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
-        {
-            return View();
-        }
-    }
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _TeamPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("http://localhost:32265/api/Staff");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultStaffDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
 }
